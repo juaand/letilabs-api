c3c7cb6c0893affa8f0bc0f3146af9e0ff443433
Andres Martinez <andresmartgonz@gmail.com>
2021-11-05 17:33:02 +0100

    gen008

---
+++ models/home/homeComponents/vigilancia.model.js
@@ -0,0 +1,48 @@
+const {Schema, model} = require('mongoose')
+
+const vigilanciaSchema = new Schema({
+    date: {
+        type: Date
+    },
+    effects: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    medicine: {
+        type: String
+    },
+    prescribed: {
+      type: String,
+      enum: ['Si', 'No']
+    },
+    sex: {
+      type: String,
+      enum: ['F', 'M']
+    }
+}, {
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+        transform: (doc, ret) => {
+            ret.id = doc._id
+            ret.date = doc.createdAt
+            delete ret._id
+            delete ret.__v
+            delete ret.createdAt
+            delete ret.updatedAt
+            return ret
+        }
+    }
+})
+
+module.exports = model("Vigilancia", vigilanciaSchema)
+
+
+
+
+
