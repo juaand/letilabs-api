6644ec6469ae654eb2dcf507f36d14ff8cb8851f
Andres Martinez <andresmartgonz@gmail.com>
2021-12-14 19:58:37 +0100

    heroku api

--- config/session.config.js
+++ config/session.config.js
@@ -3,31 +3,14 @@ const MongoStore = require('connect-mongo')(session)
 const mongoose = require('mongoose')
 const SESSION_MAX_AGE_SECONDS = Number(process.env.SESSION_MAX_AGE_SECONDS) || 60 * 60 * 24 * 7
 
-//LOCALHOST
-module.exports = session({
-  secret: process.env.SESSION_SECRET || 'Letilabsapi',
-  resave: true,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,
-    httpOnly: true,
-    maxAge: SESSION_MAX_AGE_SECONDS * 1000
-  },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: SESSION_MAX_AGE_SECONDS
-  })
-})
 
-
-//HEROKU
 // module.exports = session({
-//   secret: process.env.SESSION_SECRET || 'Mappetapi',
+//   secret: process.env.SESSION_SECRET || 'Grupoleti',
 //   resave: true,
 //   saveUninitialized: false,
 //   cookie: {
-//     sameSite: 'none',
-//     secure: process.env.SESSION_SECURE,
+//     sameSite: process.env.SESSION_SAMESITE || true,
+//     secure: process.env.SESSION_SECURE || false,
 //     httpOnly: true,
 //     maxAge: SESSION_MAX_AGE_SECONDS * 1000
 //   },
@@ -36,3 +19,37 @@ module.exports = session({
 //     ttl: SESSION_MAX_AGE_SECONDS
 //   })
 // })
+
+//LOCALHOST
+// module.exports = session({
+//   secret: process.env.SESSION_SECRET || 'Grupoleti',
+//   resave: true,
+//   saveUninitialized: false,
+//   cookie: {
+//     secure: false,
+//     httpOnly: true,
+//     maxAge: SESSION_MAX_AGE_SECONDS * 1000
+//   },
+//   store: new MongoStore({
+//     mongooseConnection: mongoose.connection,
+//     ttl: SESSION_MAX_AGE_SECONDS
+//   })
+// })
+
+
+//HEROKU
+module.exports = session({
+  secret: process.env.SESSION_SECRET || 'Grupoleti',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: 'none',
+    secure: process.env.SESSION_SECURE,
+    httpOnly: true,
+    maxAge: SESSION_MAX_AGE_SECONDS * 1000
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: SESSION_MAX_AGE_SECONDS
+  })
+})
