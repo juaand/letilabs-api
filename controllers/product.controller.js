3395e89b49c3568c4df1e708c64401e8b09ab141
Andres Martinez <andresmartgonz@gmail.com>
2021-12-21 19:46:55 +0100

    vadevecum det product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,9 +5,25 @@ const Vadevecum = require('../models/vadevecum.model')
 module.exports.getProduct = (req, res, next) => {
 const {buscar} = req.body
 
+  // const getProduct = Vadevecum.find({name: buscar})
+  // const getRandomProducts = Vadevecum.aggregate([{$sample: {size: 3}}])
+
+  // Promise.all([getProduct, getRandomProducts])
+  //   .then((data) => {
+  //     res.status(201).json(data)
+  //   })
+  //   .catch(next)
+
   Vadevecum.find({name: buscar})
-    .then((data) => {
-      res.status(201).json(data)
+    .then(response => {
+      console.log(response)
+      if (response.length) {
+        res.status(201).json(response)
+      } else {
+        res.status(201).json({
+          message: 'No se encontraron resultados'
+        })
+      }
     })
     .catch(next)
 }
