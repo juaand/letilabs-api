7af02dc440ed90e03b038b09cf22b7a6c7d25641
Andres Martinez <andresmartgonz@gmail.com>
2021-12-22 12:34:35 +0100

    vadevecum get random products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,25 +5,12 @@ const Vadevecum = require('../models/vadevecum.model')
 module.exports.getProduct = (req, res, next) => {
 const {buscar} = req.body
 
-  // const getProduct = Vadevecum.find({name: buscar})
-  // const getRandomProducts = Vadevecum.aggregate([{$sample: {size: 3}}])
+  const getProduct = Vadevecum.find({name: buscar})
+  const getRandomProducts = Vadevecum.aggregate([{$sample: {size: 3}}])
 
-  // Promise.all([getProduct, getRandomProducts])
-  //   .then((data) => {
-  //     res.status(201).json(data)
-  //   })
-  //   .catch(next)
-
-  Vadevecum.find({name: buscar})
-    .then(response => {
-      console.log(response)
-      if (response.length) {
-        res.status(201).json(response)
-      } else {
-        res.status(201).json({
-          message: 'No se encontraron resultados'
-        })
-      }
+  Promise.all([getProduct, getRandomProducts])
+    .then((data) => {
+      res.status(201).json(data)
     })
     .catch(next)
 }
