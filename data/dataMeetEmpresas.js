4e7d41bd286289e37e8e98bf5d4d8516899dea17
Andres Martinez <andresmartgonz@gmail.com>
2022-01-20 11:51:53 +0100

    data updated

--- data/dataMeetEmpresas.js
+++ data/dataMeetEmpresas.js
@@ -3,13 +3,13 @@ module.exports = [
     title: 'Conoce al talento que hace todo posible',
     button: 'Conoce a nuestra gente',
     url: "/conocenos",
-    img: './images/talento.jpg'
+    img: 'talento.jpg'
   },
   {
     title: 'Conoce la empresa homóloga de Leti en Ecuador',
     button: 'Visita Megat',
     url: 'https://megat.com.ec/',
-    img: './images/megat.jpg'
+    img: 'megat.jpg'
   }
 ]
 
