a56407889bf9a3cf532a292d630746b9f722dbb6
Andrés Martínez González <andresmartgonz@gmail.com>
2022-01-27 09:33:48 +0100

    seed pics update

--- data/bottomCtaNuestraGenteData.js
+++ data/bottomCtaNuestraGenteData.js
@@ -3,13 +3,13 @@ module.exports = [
     title: 'Conoce donde trabajamos',
     buttonTitle: 'Nuestras instalaciones',
     buttonLink: "/instalaciones",
-    img: 'nuestra-gente-instalaciones.58fe62ed.jpg'
+    img: 'nuestra-gente-instalaciones.jpg'
   },
   {
     title: 'Conoce las unidades que forman parte del grupo',
     buttonTitle: 'Nuestras empresas',
     buttonLink: "/empresas",
-    img: 'nuestra-gente-empresas.77ffe7a6.jpg'
+    img: 'nuestra-gente-empresas.jpg'
   }
 ]
 
--- data/dataMeetPeopleWorkWhitUs.js
+++ data/dataMeetPeopleWorkWhitUs.js
@@ -3,13 +3,13 @@ module.exports = [
     title: 'Contamos con los especialistas más dedicados',
     button: 'Conoce a nuestra gente',
     url: "/conocenos",
-    img: 'especialistas.e18132e0.png'
+    img: 'especialistas.png'
   },
   {
     title: 'Somos el espacio ideal para crecer',
     button: 'Trabaja con nosotros',
     url: "/trabajaconnosotros",
-    img: 'espacio.141585b2.jpg'
+    img: 'espacio.jpg'
   }
 ]
 
