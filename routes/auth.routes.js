514d541e5dad2a6fef9efe78684da5da089af081
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-02 10:56:23 +0100

    configs updated

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,6 @@
 
 const express = require("express")
 const router = express.Router()
-const upload = require('../config/cloudinary.config')
 const routeGuard = require('../middlewares/auth.middleware')
 const authController = require('../controllers/auth.controller')
 
