683744db4cc1864847cc0f11c2edab87756b7c9d
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-03 19:32:08 +0100

    bottomCta our companies page bg updated

--- data/bottomCtaIDData.js
+++ data/bottomCtaIDData.js
@@ -3,13 +3,13 @@ module.exports = [
     title: 'Impulsados por el propósito y compromiso',
     buttonTitle: 'Conoce a nuestra gente',
     buttonLink: "/nuestras-empresas",
-    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fnuestras-empresas-talento.jpg?alt=media&token=91ebea5c-76d9-4a81-98b0-9d9190e441dc'
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fiyd-equipo.jpg?alt=media&token=e96ed2bc-67d9-4925-b058-38efa0217f0e'
   },
   {
     title: 'Entérate de las novedades',
     buttonTitle: 'Leer más',
     buttonLink: "/noticias",
-    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fnuestras-empresas-megat.jpg?alt=media&token=09d6e310-613c-4092-834a-29f94876ff11'
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fiyd-novedades.jpg?alt=media&token=bc3892f2-8f97-4142-b7bc-cdb0882537d6'
   }
 ]
 
--- data/dataMeetEmpresas.js
+++ data/dataMeetEmpresas.js
@@ -2,14 +2,14 @@ module.exports = [
   {
     title: 'Conoce al talento que hace todo posible',
     button: 'Conoce a nuestra gente',
-    url: "/conocenos",
-    img: 'talento.jpg'
+    url: "/nuestra-gente",
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fnuestras-empresas-talento.jpg?alt=media&token=ff636551-ef75-4605-a438-e24fae2df6c7'
   },
   {
     title: 'Conoce la empresa homóloga de Leti en Ecuador',
     button: 'Visita Megat',
     url: 'https://megat.com.ec/',
-    img: 'megat.jpg'
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fnuestras-empresas-megat.jpg?alt=media&token=8a4221d6-9866-428f-b2c8-9e68a453f922'
   }
 ]
 
