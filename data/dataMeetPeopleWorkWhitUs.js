4bb9010e0808a0d9c48f0311aeb3ed6697d7cdaf
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-03 19:49:08 +0100

    home bottomcta bg updates

--- data/dataMeetPeopleWorkWhitUs.js
+++ data/dataMeetPeopleWorkWhitUs.js
@@ -2,14 +2,14 @@ module.exports = [
   {
     title: 'Contamos con los especialistas más dedicados',
     button: 'Conoce a nuestra gente',
-    url: "/conocenos",
-    img: 'especialistas.png'
+    url: "/nuestra-gente",
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fespecialistas.png?alt=media&token=542289cb-325c-4436-bbc5-e75a1e4310ed'
   },
   {
     title: 'Somos el espacio ideal para crecer',
     button: 'Trabaja con nosotros',
-    url: "/trabajaconnosotros",
-    img: 'espacio.jpg'
+    url: "https://www.linkedin.com/company/laboratorios-leti-s-a-v-/jobs/",
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fespacio.jpg?alt=media&token=133b4307-fccf-429c-a1ec-f97382166403'
   }
 ]
 
