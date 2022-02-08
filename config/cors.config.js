4e025e409fc9a983611ee763b7d2817de6efe8f3
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-08 13:18:03 +0100

    our purpose farmaco vigilancia

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,8 +1,7 @@
 const cors = require('cors')
 
 const corsMiddleware = cors({
-  //origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  origin: 'https://grupo-leti-fd84e.web.app',
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
   allowedHeaders: ['Content-Type'],
   credentials: true
 })
