2e3cc0f4dd163655a52b2cec2ef1d0318e625461
juan.romero <juan.romero@ip-192-168-4-34.ap-southeast-1.compute.internal>
2022-02-09 22:25:09 +0100

    our philosophy

--- controllers/ourphilosophy.controller.js
+++ controllers/ourphilosophy.controller.js
@@ -14,12 +14,46 @@ module.exports.getBannerOP = (req, res, next) => {
     .catch(next)
 }
 
+module.exports.updateBannerOP = (req, res, next) => {
+  const userRole = req.session.user.role
+  const {description, imgURL, title, id} = req.body
+
+
+  if (userRole === 'Admin') {
+    BannerOP.findByIdAndUpdate(id, req.body, {new: true})
+      .then((data) => {
+        res.status(201).json(data)
+      })
+      .catch(next)
+  } else {
+    req.session.destroy()
+    res.status(204).json({message: '¡No tiene suficientes privilegios para realizar esta acción!'})
+  }
+}
+
 module.exports.getInfoCardsOP = (req, res, next) => {
   InfoCardsOP.find()
     .then((data) => {
       res.status(201).json(data)
     })
     .catch(next)
+}
+
+module.exports.updateInfoCardsOP = (req, res, next) => {
+  const userRole = req.session.user.role
+  const {picPath, title, id} = req.body
+
+
+  if (userRole === 'Admin') {
+    InfoCardsOP.findByIdAndUpdate(id, req.body, {new: true})
+      .then((data) => {
+        res.status(201).json(data)
+      })
+      .catch(next)
+  } else {
+    req.session.destroy()
+    res.status(204).json({message: '¡No tiene suficientes privilegios para realizar esta acción!'})
+  }
 }
 
 module.exports.getLetterOP = (req, res, next) => {
@@ -30,10 +64,44 @@ module.exports.getLetterOP = (req, res, next) => {
     .catch(next)
 }
 
+module.exports.updateLetterOP = (req, res, next) => {
+  const userRole = req.session.user.role
+  const {body, imgURL, mainTitle, id} = req.body
+
+
+  if (userRole === 'Admin') {
+    LetterOP.findByIdAndUpdate(id, req.body, {new: true})
+      .then((data) => {
+        res.status(201).json(data)
+      })
+      .catch(next)
+  } else {
+    req.session.destroy()
+    res.status(204).json({message: '¡No tiene suficientes privilegios para realizar esta acción!'})
+  }
+}
+
 module.exports.getBottomOP = (req, res, next) => {
   BottomOP.find()
     .then((data) => {
       res.status(201).json(data[0])
     })
     .catch(next)
+}
+
+module.exports.updateBottomOP = (req, res, next) => {
+  const userRole = req.session.user.role
+  const {title, imgURL, description, buttonLink, buttonTitle, id} = req.body
+
+
+  if (userRole === 'Admin') {
+    BottomOP.findByIdAndUpdate(id, req.body, {new: true})
+      .then((data) => {
+        res.status(201).json(data)
+      })
+      .catch(next)
+  } else {
+    req.session.destroy()
+    res.status(204).json({message: '¡No tiene suficientes privilegios para realizar esta acción!'})
+  }
 }
--- routes/our-philosophy.routes.js
+++ routes/our-philosophy.routes.js
@@ -10,6 +10,10 @@ module.exports = router
 
 //our philosophy routes
 router.get('/bannerourphilosophy', ourPhilosophyController.getBannerOP)
+router.get('/updatebannerourphilosophy', ourPhilosophyController.updateBannerOP)
 router.get('/ourphilosophyinfocards', ourPhilosophyController.getInfoCardsOP)
+router.get('/updateourphilosophyinfocards', ourPhilosophyController.updateInfoCardsOP)
 router.get('/ourphilosophyletter', ourPhilosophyController.getLetterOP)
+router.get('/updateourphilosophyletter', ourPhilosophyController.updateLetterOP)
 router.get('/bottomourphilosophy', ourPhilosophyController.getBottomOP)
+router.get('/updatebottomourphilosophy', ourPhilosophyController.updateBottomOP)
