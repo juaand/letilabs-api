98e8a71e9a33138d67cc49d8a03a2bb5963ca1cf
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-10 11:44:39 +0100

    seed updates

--- data/unidadesNegocioEmpresasData.js
+++ data/unidadesNegocioEmpresasData.js
@@ -2,21 +2,21 @@ module.exports = [
         {
           id: 1,
           name: 'Leti',
-          logo: 'Leti',
+          logo: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fleti.svg?alt=media&token=7b074feb-97aa-4ab9-bf8a-f0981dfff556',
           url: '/leti',
           info: '<strong>Laboratorios Leti</strong> es una empresa 100% venezolana que ha ido fortaleciendo su portafolio, con áreas de productos de prescripción RX, sin prescripción OTC, de recomendación del especialista OTX, naturales FitoLeti, oncológicos, productos de nanotecnología, test genéticos, HIV que cuentan con tecnología de vanguardia y primera línea para ofrecer al venezolano medicamentos de alta calidad.'
         },
         {
           id: 2,
           name: 'Genven',
-          logo: 'Genven',
+          logo: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fgenven.svg?alt=media&token=d36a1da6-0a37-4ce4-891c-f2c850d7c0be',
           url: '/genven',
           info: '<strong>Genven Genéricos Venezolanos</strong>, es la línea de genéricos de laboratorios Leti coon más de 25 años en el Mercado Farmacéutico Venezolano. <br/><br/>Genven, le ofrece a sus pacientes terapia de primera elección en las principales líneas terapéuticas; Cardiovascular, Antiinfecciosos, Antiinflamatorios y Analgésicos. '
         },
         {
           id: 3,
           name: 'Biocontrolled',
-          logo: 'Biocontrolled',
+          logo: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fbiocontrolled.svg?alt=media&token=75a2a9f3-e198-4af0-b066-80d414f0715e',
           url: '/biocontrolled',
           info: '<strong>Biocontrolled</strong> se dedica a la investigación, desarrollo, producción de medicinas con tecnología de liberación modificada para el grupo.<br/><br/>Permite al grupo estar a la vanguardia y ofrecerle al venezolano medicamentos de altísima calidad. '
         },
