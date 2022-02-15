f9ff8e934d87027fc46ff9596229ab25ca854b23
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-15 13:37:11 +0100

    CRUD OurPhilosophy Infocards delete

--- controllers/ourphilosophy.controller.js
+++ controllers/ourphilosophy.controller.js
@@ -55,6 +55,26 @@ module.exports.updateInfoCardsOP = (req, res, next) => {
   }
 }
 
+module.exports.deleteInfoCardOP = (req, res, next) => {
+  const id = req.params.id
+  const userRole = req.session.user.role
+
+  if (userRole === 'Admin') {
+    InfoCardsOP.findByIdAndRemove(id)
+      .then(() => {
+        InfoCardsOP.find()
+          .then((data) => {
+            res.status(201).json(data)
+          })
+          .catch(next)
+      })
+      .catch(next)
+  } else {
+    req.session.destroy()
+    res.status(204).json({message: '¡No tiene suficientes privilegios para realizar esta acción!'})
+  }
+}
+
 module.exports.getLetterOP = (req, res, next) => {
   LetterOP.find()
     .then((data) => {
