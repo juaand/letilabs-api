7ee4bafd627ad7925e49d1a3b4e62dc6cfb96f1b
juan.romero <juan.romero@juanromeros-MacBook-Pro.local>
2022-02-17 21:38:07 +0100

    adding images to data

--- data/dataGallery.js
+++ data/dataGallery.js
@@ -3,31 +3,31 @@ module.exports = [
     mainTitle: 'Hoy contamos con lo mejor',
     title: 'Planta',
     desc: 'Contamos con 75.000mts2 de capacidad instalada de producción, 6 plantas con  tecnología de punta en la industria farmacéutica global, para poder elaborar hasta 120 millones de unidades al año, en 70 líneas de producción abarcando más de 22 formas galénicas',
-    imgPath: 'planta-produccion.jpg'
+    imgPath: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fplanta-produccion.jpg?alt=media&token=62680484-27ae-4b35-97d5-b1666f9f7387'
   },
   {
     mainTitle: 'Hoy contamos con lo mejor',
     title: 'Innovación',
     desc: 'Desde nuestro origen siempre hemos descubierto nuevas razones para innovar, promoviendo constantemente el desarrollo de la salud, siendo pioneros en la creación de diversas fórmulas galénicas, genéricas y de nanotecnología en la región ',
-    imgPath: 'innovacion.jpg'
+    imgPath: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Finnovacion.jpg?alt=media&token=795f334b-4818-4ac7-b3a3-fecc1a05a5d9'
   },
   {
     mainTitle: 'Hoy contamos con lo mejor',
     title: 'Portafolio',
     desc: 'Contamos con más de 600 registros en las principales líneas éticas, génericas, OTC y de fitofármacos, apalancados en una robusta cadena de distribución y soportados por un almacén robotizado con capacidad de 16 millones de unidades ',
-    imgPath: 'portafolio.jpg'
+    imgPath: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fportafolio.jpg?alt=media&token=3c11d64c-94cd-482e-aad9-9328dd520aca'
   },
   {
     mainTitle: 'Hoy contamos con lo mejor',
     title: 'Equipo',
     desc: 'Nuestras operaciones solo son posibles gracias a lo mejor de nuestro talento humano, 850 colaboradores muy involucrados y especializados en producir eficaz y oportunamente, soluciones farmacéuticas al servicio de la vida humana ',
-    imgPath: 'equipo.jpg'
+    imgPath: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fequipo.jpg?alt=media&token=48d247e4-ca4b-42de-b81b-3c59b45ca359'
   },
   {
     mainTitle: 'Hoy contamos con lo mejor',
     title: 'Networking',
     desc: 'Trabajamos de la mano de los principales proveedores del país y los referentes mundiales del sector; nos involucramos en las áreas de investigación y formación; proveemos soluciones a los distintos gremios médicos y a nuestros aliados en el área farmacéutica',
-    imgPath: 'networking.jpg'
+    imgPath: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fnetworking.jpg?alt=media&token=4b23db5d-9e4a-4e1b-acba-0585a9ccb5e0'
   }
 ]
 
--- data/homeCarousel.js
+++ data/homeCarousel.js
@@ -2,31 +2,31 @@ module.exports = [
   {    
     title: 'Nuestros productos',
     name: 'Alivet',
-    img: 'Alivet',
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Falivet.png?alt=media&token=184a8c1d-58b5-403f-b10f-29ca7242a2da',
     desc: 'Lorem ipsum dolor'
   },
   {
     title: 'Nuestros productos',
     name: 'Antux',
-    img: 'Antux',
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fantux.png?alt=media&token=d76f8638-017a-47e2-b644-d8ba68d64e64',
     desc: 'Dolor sit amet'
   },
   {
     title: 'Nuestros productos',
     name: 'Letisan',
-    img: 'Letisan',
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fletisan.png?alt=media&token=4c23db4d-f18b-4f23-a2d9-b08ccc61d543',
     desc: 'La vitamina C para toda la familia'
   },
   {
     title: 'Nuestros productos',
     name: 'Migren',
-    img: 'Migren',
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fmigren.png?alt=media&token=4c842fb7-e7aa-44e7-b359-e8776e8970c9',
     desc: 'Dolor sit amet'
   },
   {
     title: 'Nuestros productos',
     name: 'Monosulpa',
-    img: 'Monosulpa',
+    img: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fmonosulpa.png?alt=media&token=db3ad41e-9980-4e06-9ac5-1df510c0b5ed',
     desc: 'Dolor sit amet'
   }
 ]
