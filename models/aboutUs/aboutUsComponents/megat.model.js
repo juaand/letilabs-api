9297f95feb2fc9f7645a339d2afad8d2042ba5c0
Andrés Martínez González <andresmartgonz@gmail.com>
2022-03-01 12:44:27 +0100

    megat model update

--- models/aboutUs/aboutUsComponents/megat.model.js
+++ models/aboutUs/aboutUsComponents/megat.model.js
@@ -1,5 +1,5 @@
 // models/megat.model.js
-const { Schema, model} = require('mongoose')
+const {Schema, model} = require('mongoose')
 
 const megatSchema = new Schema(
   {
@@ -19,8 +19,13 @@ const megatSchema = new Schema(
       type: String,
       required: true
     },
+    logoURL: {
+      type: String,
+      required: true,
+      default: 'https://firebasestorage.googleapis.com/v0/b/grupo-leti-fd84e.appspot.com/o/images%2Fmegat.svg?alt=media&token=effe1cb4-00ed-4c17-b446-47129e124dfc'
+    }
   },
-  { timestamps: true }
+  {timestamps: true}
 )
 
 module.exports = model('Megat', megatSchema)
