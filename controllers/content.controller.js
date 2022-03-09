f0c54d0aae037a1627a66a0d809b0bab479af0f3
Andrés Martínez González <andresmartgonz@gmail.com>
2022-03-09 13:43:59 +0100

    content reload

--- controllers/content.controller.js
+++ controllers/content.controller.js
@@ -1,19 +1,18 @@
 // controllers/content.controllers.js
 const SiteContent = require('../models/siteContent.model')
 
+
 module.exports.createContent = (req, res, next) => {
   const {name, url, content} = req.body
 
   const dataArr = new Set(content)
-
-  console.log(dataArr)
 
   SiteContent.deleteMany()
 
   SiteContent.find({name})
     .then(response => {
       if (response.length) {
-        SiteContent.findOneAndUpdate({name}, {$set: {content: dataArr[0]}})
+        SiteContent.findOneAndUpdate({name}, {$set: {content}})
           .then(() => {
             res.status(201).json({
               message: 'Contenido ya existe'
@@ -21,6 +20,10 @@ module.exports.createContent = (req, res, next) => {
           })
           .catch(next)
       } else {
+        // SiteContent.create({name, url, content}).then((newContent) => {
+        //   res.status(201).json(newContent)
+        // })
+        //   .catch(next)
         dataArr.forEach(el => {
           SiteContent.create({name, url, content: el})
             .then(() => {
