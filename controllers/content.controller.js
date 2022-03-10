d86bac374fc9db1fea47e9a3ad90faa8363a76fa
juan.romero <juan.romero@thoughtworks.com>
2022-03-10 16:45:05 +0100

    update flow =)

--- controllers/content.controller.js
+++ controllers/content.controller.js
@@ -5,22 +5,22 @@ const SiteContent = require('../models/siteContent.model')
 module.exports.createContent = (req, res, next) => {
   const {name, url, content} = req.body
 
-  const dataArr = new Set(content)
+  const dataArr = [... new Set(content)];
 
-  console.log(content)
+  console.log(dataArr)
   console.log(name)
   Promise.all([
     SiteContent.deleteMany({"name": name})
   ]
-  )
-
-        dataArr.forEach(el => {
-          SiteContent.create({name, url, content: el})
-            .then(() => {
-              res.status(201).json({
-                message: 'Contenidos creados exitosamente'
-              })
-            })
-            .catch(next)
+  ).then(() => {
+    dataArr.forEach(el => {
+      SiteContent.create({name, url, content: el})
+        .then(() => {
+          res.status(201).json({
+            message: 'Contenidos creados exitosamente'
+          })
         })
-      }
+        .catch(next)
+    })
+  })
+}
