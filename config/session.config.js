c36aa93a97bf573810fed3dac3a37793ed9d17f5
Andrés Martínez González <andresmartgonz@gmail.com>
2023-06-07 18:35:46 +0200

    heroku conf

--- config/session.config.js
+++ config/session.config.js
@@ -21,12 +21,29 @@ const SESSION_MAX_AGE_SECONDS = Number(process.env.SESSION_MAX_AGE_SECONDS) || 6
 // })
 
 //LOCALHOST
- module.exports = session({
+//  module.exports = session({
+//     secret: process.env.SESSION_SECRET || 'Grupoleti',
+//     resave: true,
+//     saveUninitialized: false,
+//     cookie: {
+//       secure: false,
+//       httpOnly: true,
+//       maxAge: SESSION_MAX_AGE_SECONDS * 1000
+//     },
+//     store: new MongoStore({
+//       mongooseConnection: mongoose.connection,
+//       ttl: SESSION_MAX_AGE_SECONDS
+//     })
+//   })
+
+//HEROKU
+  module.exports = session({
     secret: process.env.SESSION_SECRET || 'Grupoleti',
     resave: true,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      sameSite: 'none',
+      secure: process.env.SESSION_SECURE,
       httpOnly: true,
       maxAge: SESSION_MAX_AGE_SECONDS * 1000
     },
@@ -35,20 +52,3 @@ const SESSION_MAX_AGE_SECONDS = Number(process.env.SESSION_MAX_AGE_SECONDS) || 6
       ttl: SESSION_MAX_AGE_SECONDS
     })
   })
-
-//HEROKU
-  // module.exports = session({
-  //   secret: process.env.SESSION_SECRET || 'Grupoleti',
-  //   resave: true,
-  //   saveUninitialized: false,
-  //   cookie: {
-  //     sameSite: 'none',
-  //     secure: process.env.SESSION_SECURE,
-  //     httpOnly: true,
-  //     maxAge: SESSION_MAX_AGE_SECONDS * 1000
-  //   },
-  //   store: new MongoStore({
-  //     mongooseConnection: mongoose.connection,
-  //     ttl: SESSION_MAX_AGE_SECONDS
-  //   })
-  // })
