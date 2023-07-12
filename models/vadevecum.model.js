b22c51ea5ce2048615568e997653c99bdb2c9ef5
Juan Romero <juan.romero@thoughtworks.com>
2023-07-12 14:56:55 +0200

    remove unnecessary _eng from model

--- models/vadevecum.model.js
+++ models/vadevecum.model.js
@@ -7,15 +7,7 @@ const vadevecumSchema = new Schema(
             type: String,
             required: true
         },
-        line_eng: {
-            type: String,
-            required: true
-        },
         name: {
-            type: String,
-            required: true
-        },
-        name_eng: {
             type: String,
             required: true
         },
@@ -23,15 +15,7 @@ const vadevecumSchema = new Schema(
             type: String,
             required: true
         },
-        active_principle_eng: {
-            type: String,
-            required: true
-        },
         composition: {
-            type: String,
-            required: true
-        },
-        composition_eng: {
             type: String,
             required: true
         },
@@ -39,15 +23,7 @@ const vadevecumSchema = new Schema(
             type: String,
             required: true
         },
-        indication_eng: {
-            type: String,
-            required: true
-        },
         posology: {
-            type: String,
-            required: true
-        },
-        posology_eng: {
             type: String,
             required: true
         },
@@ -55,15 +31,7 @@ const vadevecumSchema = new Schema(
             type: String,
             required: true
         },
-        presentation_eng: {
-            type: String,
-            required: true
-        },
         health_register: {
-            type: String,
-            required: true
-        },
-        health_eng: {
             type: String,
             required: true
         },
@@ -71,26 +39,13 @@ const vadevecumSchema = new Schema(
             type: [String],
             required: true
         },
-        therapeutic_group_eng: {
-            type: [String],
-            required: true
-        },
         category: {
-            type: String,
-        },
-        category_eng: {
             type: String,
         },
         tv_spot: {
             type: [String],
         },
-        tv_spot_eng: {
-            type: [String],
-        },
         trademarks: {
-            type: String,
-        },
-        trademarks_eng: {
             type: String,
         },
         show_in_home: {
@@ -113,15 +68,7 @@ const vadevecumSchema = new Schema(
             type: String,
             default: 'Sin información de vida útil de este medicamento.'
         },
-        util_life_eng: {
-            type: String,
-            default: 'Sin información de vida útil de este medicamento.'
-        },
         cpe: {
-            type: String,
-            default: 'Sin información del CPE de este medicamento.'
-        },
-        cpe_eng: {
             type: String,
             default: 'Sin información del CPE de este medicamento.'
         },
@@ -129,15 +76,7 @@ const vadevecumSchema = new Schema(
             type: String,
             default: '- Sin información del uso de este medicamento.'
         },
-        how_to_use_eng: {
-            type: String,
-            default: '- Sin información del uso de este medicamento.'
-        },
         contraindications: {
-            type: String,
-            default: '- Sin información de las contraindicaciones de este medicamento.'
-        },
-        contraindications_eng: {
             type: String,
             default: '- Sin información de las contraindicaciones de este medicamento.'
         },
@@ -145,18 +84,10 @@ const vadevecumSchema = new Schema(
             type: String,
             default: '- Sin información de las reacciones adversas de este medicamento.'
         },
-        adverse_reactions_eng: {
-            type: String,
-            default: '- Sin información de las reacciones adversas de este medicamento.'
-        },
         prospect: {
             type: String,
             default: '- Sin información de prospecto.'
         },
-        prospect_eng: {
-            type: String,
-            default: '- Sin información de prospecto.'
-        }
     },
     {timestamps: true}
 )
