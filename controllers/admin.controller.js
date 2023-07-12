b62eac732fe2c0c45b6f88844de8124245793fc8
Juan Romero <juan.romero@thoughtworks.com>
2023-07-12 14:45:04 +0200

    remove unnecessary _eng 3

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -2022,7 +2022,7 @@ module.exports.deleteProduct = (req, res, next) => {
 module.exports.updateProduct = (req, res, next) => {
   const id = req.params.id
   const userRole = req.session.user.role
-  const {name, picPath, QRpath, line, composition, health_register, active_principle, posology, presentation, indication, therapeutic_group, category, util_life, cpe, how_to_use, contraindications, adverse_reactions, prospect, show_in_products, name_eng, line_eng, composition_eng, health_register_eng, active_principle_eng, posology_eng, presentation_eng, indication_eng, therapeutic_group_eng, category_eng, util_life_eng, cpe_eng, how_to_use_eng, contraindications_eng, adverse_reactions_eng, prospect_eng} = req.body
+  const {name, picPath, QRpath, line, composition, health_register, active_principle, posology, presentation, indication, therapeutic_group, category, util_life, cpe, how_to_use, contraindications, adverse_reactions, prospect, show_in_products} = req.body
 
   if (userRole === 'Admin') {
     Vadevecum.findByIdAndUpdate(id, req.body, {new: true})
