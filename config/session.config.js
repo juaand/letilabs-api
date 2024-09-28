d33c4ae5b0e897d22bf3cbe7d314c81d298b0319
Juan Romero <ironhackerjuan@gmail.com>
2024-09-28 10:34:53 +0200

    change session config

--- config/session.config.js
+++ config/session.config.js
@@ -4,37 +4,37 @@ const mongoose = require('mongoose')
 const SESSION_MAX_AGE_SECONDS = Number(process.env.SESSION_MAX_AGE_SECONDS) || 60 * 60 * 24 * 7
 
 
-// module.exports = session({
-//   secret: process.env.SESSION_SECRET || 'Grupoleti',
-//   resave: true,
-//   saveUninitialized: false,
-//   cookie: {
-//     sameSite: process.env.SESSION_SAMESITE || true,
-//     secure: process.env.SESSION_SECURE || false,
-//     httpOnly: true,
-//     maxAge: SESSION_MAX_AGE_SECONDS * 1000
-//   },
-//   store: new MongoStore({
-//     mongooseConnection: mongoose.connection,
-//     ttl: SESSION_MAX_AGE_SECONDS
-//   })
-// })
+module.exports = session({
+  secret: process.env.SESSION_SECRET || 'Grupoleti',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: process.env.SESSION_SAMESITE || true,
+    secure: process.env.SESSION_SECURE || false,
+    httpOnly: true,
+    maxAge: SESSION_MAX_AGE_SECONDS * 1000
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: SESSION_MAX_AGE_SECONDS
+  })
+})
 
 //LOCALHOST
- module.exports = session({
-    secret: process.env.SESSION_SECRET || 'Grupoleti',
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-      secure: false,
-      httpOnly: true,
-      maxAge: SESSION_MAX_AGE_SECONDS * 1000
-    },
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl: SESSION_MAX_AGE_SECONDS
-    })
-  })
+//  module.exports = session({
+//     secret: process.env.SESSION_SECRET || 'Grupoleti',
+//     resave: true,
+//     saveUninitialized: false,
+//     cookie: {
+//       secure: false,
+//       httpOnly: true,
+//       maxAge: SESSION_MAX_AGE_SECONDS * 1000
+//     },
+//     store: new MongoStore({
+//       mongooseConnection: mongoose.connection,
+//       ttl: SESSION_MAX_AGE_SECONDS
+//     })
+//   })
 
 //HEROKU
   // module.exports = session({
